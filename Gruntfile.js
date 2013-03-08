d31b711d7d19868a5513afbd2fb29424635b0e01
Sarah Spikes <sdspikes@cs.stanford.edu>
2013-03-07 17:32:26 -0800

    uglify in a way that actually allows you to still run nodeWrapper

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,19 @@
 module.exports = function(grunt) {
+  var u = require('underscore');
+  var uglifyObj = {
+    options: {
+      banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    }
+  };
+  u.each(grunt.file.expandMapping('*.js', 'build', {cwd: 'src/'}), function (fileMap) {
+    uglifyObj[fileMap.src] = fileMap;
+  });
+
 
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    }
+    uglify: uglifyObj
   });
 
   // Load the plugin that provides the "uglify" task.
