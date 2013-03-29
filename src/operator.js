f1211413234f90a3abc29c1a81d456511cf31dd5
Sarah Spikes <sdspikes@cs.stanford.edu>
2013-03-28 21:39:51 -0700

    pure formatting change

--- src/operator.js
+++ src/operator.js
@@ -12,10 +12,10 @@ var operator = (function() {
 	/**
 	 * Object that knows how to evaluate all the evaluable operators.
 	 * @constructor
-	 * @param evaluate       {Function}           Defines how to perform the operation given two
-	 *                                            Expression operands.
-	 * @param evaluateValues {Function}           Defines how to perform the operation given two
-	 *                                            numbers as operands.
+	 * @param evaluate       {Function} Defines how to perform the operation given two Expression
+	 *                                  operands.
+	 * @param evaluateValues {Function} Defines how to perform the operation given two numbers as
+	 *                                  operands.
 	 */
 	function Operator(evaluate, evaluateValues) {
 		this.evaluate = evaluate;
@@ -72,208 +72,220 @@ var operator = (function() {
 	 * @dict
 	 */
 	self = {
-		'^' : new Operator(function(lhs, rhs) {
-					if (lhs.type !== 'number' || rhs.type !== 'number' ||
+		'^' : new Operator(
+			function(lhs, rhs) {
+				if (lhs.type !== 'number' || rhs.type !== 'number' ||
+					fractionUtils.isValueRadical(rhs.value)) {
+					return expression.createCompoundExpression(lhs, rhs, '^');
+				}
+				if (lhs.value === 1) { return lhs; }
+				return operator['^'].evaluateValues(lhs.value, rhs.value);
+			}, function(lhsValue, rhsValue) {
+				if (fractionUtils.isValueFraction(rhsValue)) {
+					var result = self.simplifyRadicalIntoNode(fractionUtils.createRadicalValue(
+						lhsValue, rhsValue, false));
+					return result;
+				}
+				var resultValue = 1;
+				if (fractionUtils.isValueFraction(lhsValue)) {
+					return operator['/'].evaluate(
+						operator['^'].evaluateValues(
+							lhsValue.top, rhsValue),
+						operator['^'].evaluateValues(
+							lhsValue.bottom, rhsValue));
+				} else if (fractionUtils.isValueRadical(lhsValue)) {
+					return self.simplifyRadicalIntoNode(fractionUtils.createRadicalValue(
+						lhsValue.radicand, operator['*'].evaluateValues(
+							lhsValue.power, rhsValue).value, false));
+				} else if (fractionUtils.isNegative(rhsValue)) {
+					resultValue = fractionUtils.createFractionValue(
+						1, Math.pow(lhsValue, -1*rhsValue));
+				} else {
+					resultValue = Math.pow(lhsValue, rhsValue);
+				}
+				return expression.createSimpleExpression('number', resultValue);
+			}
+		),
+		'/' : new Operator(
+			function(lhs, rhs) {
+				if (!(lhs.numeric && rhs.numeric) && (!lhs.simplified || !rhs.simplified)) {
+					return expression.createCompoundExpression(lhs, rhs, '/');
+				}
+				if (lhs.type !== 'number' || rhs.type !== 'number') {
+					var result = expression.createCompoundExpression(lhs, rhs, '/');
+					result.simplified = true;
+					return result;
+				}
+				return operator['/'].evaluateValues(lhs.value, rhs.value);
+			}, function(lhsValue, rhsValue) {
+				var resultValue = null;
+				if (fractionUtils.isValueRadical(rhsValue)) {
+					return operator['*'].evaluate(
+						expression.createSimpleExpression('number', lhsValue),
+						self.rationalizeDenominator(expression.createSimpleExpression('number',
+							fractionUtils.invertFraction(rhsValue))));
+				}
+				if (fractionUtils.isValueRadical(lhsValue)) {
+					var result = expression.createCompoundExpression(
+						expression.createSimpleExpression('number', lhsValue),
+						expression.createSimpleExpression('number', rhsValue),
+						'/');
+					result.simplified = true;
+					return result;
+				}
+				if (fractionUtils.isValueFraction(rhsValue)) {
+					return operator['*'].evaluateValues(
+						lhsValue, fractionUtils.invertFraction(rhsValue));
+				} else if (fractionUtils.isValueFraction(lhsValue)) {
+					return operator['/'].evaluate(
+						makeNumber(lhsValue.top),
+						operator['*'].evaluateValues(
+							lhsValue.bottom, rhsValue));
+				} else {
+					resultValue = fractionUtils.simplifyFraction(
+						fractionUtils.createFractionValue(lhsValue, rhsValue));
+				}
+				return expression.createSimpleExpression('number', resultValue);
+			}
+		),
+		'*' : new Operator(
+			function(lhs, rhs) {
+				var node = expression.createCompoundExpression(lhs, rhs, '*');
+				node = pullOutRadical(node);
+				if (node.radical !== null) {
+					var orig = node.radical;
+					var combined = combineRadicals(node.radical);
+					node = multiplyNodesMaybeNull(node.rest, combined);
+					if (orig.syntacticEquals(combined) || node.type !== 'compound') {
+						return node;
+					}
+					lhs = node.lhs;
+					rhs = node.rhs;
+				}
+				if (!lhs.numeric || !rhs.numeric) {
+					return expression.createCompoundExpression(lhs, rhs, '*');
+				}
+				if (lhs.type === 'compound') {
+					if (getPrecedence(lhs.op) === 2) {
+						// (2/x)*3 = (2*3)/x
+						if (lhs.lhs.type === 'number' &&
+							!fractionUtils.isValueRadical(lhs.lhs.value)) {
+							return operator[lhs.op].evaluate(
+								operator['*'].evaluate(lhs.lhs, rhs),
+								lhs.rhs);
+						}
+						// (x/2)*3 = x*(3/2)
+						if (lhs.rhs.type === 'number' &&
+							!fractionUtils.isValueRadical(lhs.rhs.value)) {
+							return operator['*'].evaluate(lhs.lhs,
+								operator[lhs.op].evaluate(rhs, lhs.rhs));
+						}
+					}
+				}
+				if (rhs.type === 'compound') {
+					if (getPrecedence(rhs.op) === 2) {
+						// 2*(3/x) = (2*3)/x
+						if (rhs.lhs.type === 'number' &&
+							!fractionUtils.isValueRadical(rhs.lhs.value)) {
+							return operator[rhs.op].evaluate(
+								operator['*'].evaluate(lhs, rhs.lhs),
+								rhs.rhs);
+						}
+						// 2*(x/3) = (2/3)*x
+						if (rhs.rhs.type === 'number' &&
+							!fractionUtils.isValueRadical(rhs.rhs.value)) {
+							return operator['*'].evaluate(
+								operator[rhs.op].evaluate(lhs, rhs.rhs),
+								rhs.lhs);
+						}
+					}
+				}
+				if (lhs.type !== 'number' || rhs.type !== 'number') {
+					return expression.createCompoundExpression(lhs, rhs, '*');
+				}
+				return operator['*'].evaluateValues(lhs.value, rhs.value);
+			}, function(lhsValue, rhsValue) {
+				var resultValue = null;
+				if (fractionUtils.isValueRadical(lhsValue) && !lhsValue.simplified) {
+					return operator['*'].evaluate(
+						self.simplifyRadicalIntoNode(lhsValue),
+						makeNumber(rhsValue));
+				}
+				if (fractionUtils.isValueRadical(rhsValue) && !rhsValue.simplified) {
+					return operator['*'].evaluate(makeNumber(lhsValue),
+						self.simplifyRadicalIntoNode(rhsValue));
+				}
+				if (fractionUtils.isValueRadical(lhsValue) ||
+					fractionUtils.isValueRadical(rhsValue)) {
+					return combineRadicals(expression.createCompoundExpression(
+						makeNumber(lhsValue), makeNumber(rhsValue), '*'));
+				}
+				return expression.createSimpleExpression('number',
+					fractionUtils.multiplyFractions(lhsValue, rhsValue));
+			}
+		),
+		'-' : new Operator(
+			function(lhs, rhs) {
+				if (lhs.type !== 'number' || rhs.type !== 'number' ||
+					(fractionUtils.isValueRadical(lhs.value) ||
+						fractionUtils.isValueRadical(rhs.value))) {
+					return expression.createCompoundExpression(lhs, rhs, '-');
+				}
+				return operator['-'].evaluateValues(lhs.value, rhs.value);
+			}, function(lhsValue, rhsValue) {
+				var resultValue = null;
+				if (!fractionUtils.isValueFraction(lhsValue) &&
+					!fractionUtils.isValueFraction(rhsValue)) {
+					resultValue = lhsValue - rhsValue;
+				} else {
+					return operator['+'].evaluateValues(lhsValue,
+						this.unaryEvaluateValue(rhsValue).value);
+				}
+				return expression.createSimpleExpression('number', resultValue);
+			}
+		),
+		'+' : new Operator(
+			function(lhs, rhs) {
+				if (lhs.type !== 'number' || rhs.type !== 'number') {
+					return expression.createCompoundExpression(lhs, rhs, '+');
+				} else if (fractionUtils.isValueRadical(lhs.value) ||
 						fractionUtils.isValueRadical(rhs.value)) {
-						return expression.createCompoundExpression(lhs, rhs, '^');
+					if (lhs.syntacticEquals(rhs)) {
+						var newNode = expression.createCompoundExpression(
+							makeNumber(2), lhs, '*');
+						newNode.simplified = true;
+						return newNode;
 					}
-					if (lhs.value === 1) { return lhs; }
-					return operator['^'].evaluateValues(lhs.value, rhs.value);
-				}, function(lhsValue, rhsValue) {
-					if (fractionUtils.isValueFraction(rhsValue)) {
-						var result = self.simplifyRadicalIntoNode(fractionUtils.createRadicalValue(
-							lhsValue, rhsValue, false));
-						return result;
+					return expression.createCompoundExpression(lhs, rhs, '+');
+				}
+				return operator['+'].evaluateValues(lhs.value, rhs.value);
+			}, function(lhsValue, rhsValue) {
+				var resultValue = null;
+				if (!fractionUtils.isValueFraction(lhsValue) &&
+					!fractionUtils.isValueFraction(rhsValue)) {
+					resultValue = lhsValue + rhsValue;
+				} else {
+					if (!fractionUtils.isValueFraction(lhsValue)) {
+						lhsValue = fractionUtils.createFractionValue(lhsValue, 1);
 					}
-					var resultValue = 1;
-					if (fractionUtils.isValueFraction(lhsValue)) {
-						return operator['/'].evaluate(
-							operator['^'].evaluateValues(
-								lhsValue.top, rhsValue),
-							operator['^'].evaluateValues(
-								lhsValue.bottom, rhsValue));
-					} else if (fractionUtils.isValueRadical(lhsValue)) {
-						return self.simplifyRadicalIntoNode(fractionUtils.createRadicalValue(
-							lhsValue.radicand, operator['*'].evaluateValues(
-								lhsValue.power, rhsValue).value, false));
-					} else if (fractionUtils.isNegative(rhsValue)) {
-						resultValue = fractionUtils.createFractionValue(
-							1, Math.pow(lhsValue, -1*rhsValue));
-					} else {
-						resultValue = Math.pow(lhsValue, rhsValue);
+					if (!fractionUtils.isValueFraction(rhsValue)) {
+						rhsValue = fractionUtils.createFractionValue(rhsValue, 1);
 					}
-					return expression.createSimpleExpression('number', resultValue);
-				}),
-		'/' : new Operator(function(lhs, rhs) {
-					if (!(lhs.numeric && rhs.numeric) && (!lhs.simplified || !rhs.simplified)) {
-						return expression.createCompoundExpression(lhs, rhs, '/');
-					}
-					if (lhs.type !== 'number' || rhs.type !== 'number') {
-						var result = expression.createCompoundExpression(lhs, rhs, '/');
-						result.simplified = true;
-						return result;
-					}
-					return operator['/'].evaluateValues(lhs.value, rhs.value);
-				}, function(lhsValue, rhsValue) {
-					var resultValue = null;
-					if (fractionUtils.isValueRadical(rhsValue)) {
-						return operator['*'].evaluate(
-							expression.createSimpleExpression('number', lhsValue),
-							self.rationalizeDenominator(expression.createSimpleExpression('number',
-								fractionUtils.invertFraction(rhsValue))));
-					}
-					if (fractionUtils.isValueRadical(lhsValue)) {
-						var result = expression.createCompoundExpression(
-							expression.createSimpleExpression('number', lhsValue),
-							expression.createSimpleExpression('number', rhsValue),
-							'/');
-						result.simplified = true;
-						return result;
-					}
-					if (fractionUtils.isValueFraction(rhsValue)) {
-						return operator['*'].evaluateValues(
-							lhsValue, fractionUtils.invertFraction(rhsValue));
-					} else if (fractionUtils.isValueFraction(lhsValue)) {
-						return operator['/'].evaluate(
-							makeNumber(lhsValue.top),
-							operator['*'].evaluateValues(
-								lhsValue.bottom, rhsValue));
-					} else {
-						resultValue = fractionUtils.simplifyFraction(
-							fractionUtils.createFractionValue(lhsValue, rhsValue));
-					}
-					return expression.createSimpleExpression('number', resultValue);
-				}),
-		'*' : new Operator(function(lhs, rhs) {
-					var node = expression.createCompoundExpression(lhs, rhs, '*');
-					node = pullOutRadical(node);
-					if (node.radical !== null) {
-						var orig = node.radical;
-						var combined = combineRadicals(node.radical);
-						node = multiplyNodesMaybeNull(node.rest, combined);
-						if (orig.syntacticEquals(combined) || node.type !== 'compound') {
-							return node;
-						}
-						lhs = node.lhs;
-						rhs = node.rhs;
-					}
-					if (!lhs.numeric || !rhs.numeric) {
-						return expression.createCompoundExpression(lhs, rhs, '*');
-					}
-					if (lhs.type === 'compound') {
-						if (getPrecedence(lhs.op) === 2) {
-							// (2/x)*3 = (2*3)/x
-							if (lhs.lhs.type === 'number' &&
-								!fractionUtils.isValueRadical(lhs.lhs.value)) {
-								return operator[lhs.op].evaluate(
-									operator['*'].evaluate(lhs.lhs, rhs),
-									lhs.rhs);
-							}
-							// (x/2)*3 = x*(3/2)
-							if (lhs.rhs.type === 'number' &&
-								!fractionUtils.isValueRadical(lhs.rhs.value)) {
-								return operator['*'].evaluate(lhs.lhs,
-									operator[lhs.op].evaluate(rhs, lhs.rhs));
-							}
-						}
-					}
-					if (rhs.type === 'compound') {
-						if (getPrecedence(rhs.op) === 2) {
-							// 2*(3/x) = (2*3)/x
-							if (rhs.lhs.type === 'number' &&
-								!fractionUtils.isValueRadical(rhs.lhs.value)) {
-								return operator[rhs.op].evaluate(
-									operator['*'].evaluate(lhs, rhs.lhs),
-									rhs.rhs);
-							}
-							// 2*(x/3) = (2/3)*x
-							if (rhs.rhs.type === 'number' &&
-								!fractionUtils.isValueRadical(rhs.rhs.value)) {
-								return operator['*'].evaluate(
-									operator[rhs.op].evaluate(lhs, rhs.rhs),
-									rhs.lhs);
-							}
-						}
-					}
-					if (lhs.type !== 'number' || rhs.type !== 'number') {
-						return expression.createCompoundExpression(lhs, rhs, '*');
-					}
-					return operator['*'].evaluateValues(lhs.value, rhs.value);
-				}, function(lhsValue, rhsValue) {
-					var resultValue = null;
-					if (fractionUtils.isValueRadical(lhsValue) && !lhsValue.simplified) {
-						return operator['*'].evaluate(
-							self.simplifyRadicalIntoNode(lhsValue),
-							makeNumber(rhsValue));
-					}
-					if (fractionUtils.isValueRadical(rhsValue) && !rhsValue.simplified) {
-						return operator['*'].evaluate(makeNumber(lhsValue),
-							self.simplifyRadicalIntoNode(rhsValue));
-					}
-					if (fractionUtils.isValueRadical(lhsValue) ||
-						fractionUtils.isValueRadical(rhsValue)) {
-						return combineRadicals(expression.createCompoundExpression(
-							makeNumber(lhsValue), makeNumber(rhsValue), '*'));
-					}
-					return expression.createSimpleExpression('number',
-						fractionUtils.multiplyFractions(lhsValue, rhsValue));
-				}),
-		'-' : new Operator(function(lhs, rhs) {
-					if (lhs.type !== 'number' || rhs.type !== 'number' ||
-						(fractionUtils.isValueRadical(lhs.value) ||
-							fractionUtils.isValueRadical(rhs.value))) {
-						return expression.createCompoundExpression(lhs, rhs, '-');
-					}
-					return operator['-'].evaluateValues(lhs.value, rhs.value);
-				}, function(lhsValue, rhsValue) {
-					var resultValue = null;
-					if (!fractionUtils.isValueFraction(lhsValue) &&
-						!fractionUtils.isValueFraction(rhsValue)) {
-						resultValue = lhsValue - rhsValue;
-					} else {
-						return operator['+'].evaluateValues(lhsValue,
-							this.unaryEvaluateValue(rhsValue).value);
-					}
-					return expression.createSimpleExpression('number', resultValue);
-				}),
-		'+' : new Operator(function(lhs, rhs) {
-					if (lhs.type !== 'number' || rhs.type !== 'number') {
-						return expression.createCompoundExpression(lhs, rhs, '+');
-					} else if (fractionUtils.isValueRadical(lhs.value) ||
-							fractionUtils.isValueRadical(rhs.value)) {
-						if (lhs.syntacticEquals(rhs)) {
-							var newNode = expression.createCompoundExpression(
-								makeNumber(2), lhs, '*');
-							newNode.simplified = true;
-							return newNode;
-						}
-						return expression.createCompoundExpression(lhs, rhs, '+');
-					}
-					return operator['+'].evaluateValues(lhs.value, rhs.value);
-				}, function(lhsValue, rhsValue) {
-					var resultValue = null;
-					if (!fractionUtils.isValueFraction(lhsValue) &&
-						!fractionUtils.isValueFraction(rhsValue)) {
-						resultValue = lhsValue + rhsValue;
-					} else {
-						if (!fractionUtils.isValueFraction(lhsValue)) {
-							lhsValue = fractionUtils.createFractionValue(lhsValue, 1);
-						}
-						if (!fractionUtils.isValueFraction(rhsValue)) {
-							rhsValue = fractionUtils.createFractionValue(rhsValue, 1);
-						}
-						resultValue = fractionUtils.addFractions(lhsValue, rhsValue);
-					}
-					return expression.createSimpleExpression('number', resultValue);
-				}),
-		'\\sqrt' : new Operator(function(lhs, rhs) {
-					return operator['^'].evaluate(rhs,
-						operator['/'].evaluateValues(1, lhs.value));
-				}, function(lhsValue, rhsValue) {
-					// Assumes lhs is the root, and rhs is the value under the sqrt.
-					return operator['^'].evaluate(makeNumber(rhsValue),
-						operator['/'].evaluateValues(1, lhsValue));
-				}),
+					resultValue = fractionUtils.addFractions(lhsValue, rhsValue);
+				}
+				return expression.createSimpleExpression('number', resultValue);
+			}
+		),
+		'\\sqrt' : new Operator(
+			function(lhs, rhs) {
+				return operator['^'].evaluate(rhs,
+					operator['/'].evaluateValues(1, lhs.value));
+			}, function(lhsValue, rhsValue) {
+				// Assumes lhs is the root, and rhs is the value under the sqrt.
+				return operator['^'].evaluate(makeNumber(rhsValue),
+					operator['/'].evaluateValues(1, lhsValue));
+			}
+		),
 		'|' : new Operator(),
 		rationalizeDenominator: function(node) {
 			if (node.type === 'number' && fractionUtils.isValueFraction(node.value) &&
